3b14cdccb4c96ed9fee64fe3f75ddf2803171315
Michael A. Gulla <mgulla@krysson.com>
2022-06-16 13:00:29 +0000

    test commit

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          mess with, but dont turn off the yarl server
+           <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
